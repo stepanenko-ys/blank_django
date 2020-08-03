dd0a8683feba70a2d3d45e4a387f1373e18f40dc
Евгений Степаненко <ys.stepanenko@gmail.com>
2020-08-04 00:12:53 +0300

    Updated project structure

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,6 +3,4 @@ from django.shortcuts import render
 
 def main(request):
 
-    print("1_2_3_4_5")
-
     return render(request, 'main/main.html', locals())
--- settings/config/base.py
+++ settings/config/base.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'settings.urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
